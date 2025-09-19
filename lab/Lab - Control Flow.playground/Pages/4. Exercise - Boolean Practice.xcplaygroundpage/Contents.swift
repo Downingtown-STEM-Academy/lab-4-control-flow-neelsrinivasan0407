644ddb9f7b4734e6f4a6cd18c492d54b03940c47
neelsrinivasan0407 <nsrinivasa01@student.dasd.org>
2025-09-19 10:59:15 -0400

    code

--- lab/Lab - Control Flow.playground/Pages/4. Exercise - Boolean Practice.xcplaygroundpage/Contents.swift
+++ lab/Lab - Control Flow.playground/Pages/4. Exercise - Boolean Practice.xcplaygroundpage/Contents.swift
@@ -13,7 +13,12 @@
 let hasFish = true
 let hasPizza = false
 let hasVegan = true
-
+if hasVegan && (hasFish || hasPizza) {
+    print("Lets go!")
+}
+else {
+    print("Sorry, we'll have to think of somewhere else.")
+}
 
 /*:
  Imagine you're trying to decide whether or not to go on a walk. You decide that you'll go on a walk if it's not raining or if it's 82 degress or warmer and NOT windy. Create a constant `walkingWeather` that is equal to an expression that evaluates to a boolean indicating whether or not the weather is nice enough for you to go for a walk. Write an if statement that will print "I'm going for a walk!" if the weather is nice. (1 points)
@@ -21,23 +26,32 @@ let hasVegan = true
  > Remove the comment from each set of conditions in turn and create a print statement that says something like: "I will go for a walk for the first set of conditions" for all 3 conditions to ensure that your code works. Make sure you comment out the other 2 sets each time!
  
  */
-
+/*
 //Set 1
 let temp = 82
 let isRaining = true
 let isWindy = false
-/* Set 2
+*/
+/*Set 2
 let temp = 82
 let isRaining = true
 let isWindy = true
 */
-/* Set 3
+//Set 3
 let temp = 75
 let isRaining = false
 let isWindy = true
-*/
 
-//insert your code here!!
 
-//insert print statements here!!
+let walkingWeather : Bool
+if (isRaining || temp >= 82 && !isWindy) {
+    walkingWeather = true
+}
+else{
+    walkingWeather = false
+}
+
+if (walkingWeather) {
+    print("I'm going for a walk!")
+}
 //: [Previous](@previous)  |  page 4 of 9  |  [Next](@next)
